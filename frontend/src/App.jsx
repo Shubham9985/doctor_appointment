af55ee7d11647c4532490a3723bf749d2c2eb237
Shubham <shubham9985kumar@gmail.com>
2025-07-15 17:12:27 +0530

    adding header and navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,30 @@
- import React from 'react'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Doctors from './pages/Doctors.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import MyProfile from './pages/MyProfile.jsx'
+import MyAppointments from './pages/MyAppointments.jsx'
+import Appointment from './pages/Appointment.jsx'
+import Navbar from './components/Navbar.jsx'
  
  const App = () => {
    return (
-     <div>
-      App
+     <div className='mx-4 sm:mx-[10%] '>
+      <Navbar /> {/* mounted outside so that it will outside all the  route pages */}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
+      </Routes>
 
      </div>
    )
